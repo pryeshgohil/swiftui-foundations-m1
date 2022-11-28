0e0f98fd19af748503dcf61145c4ee950e4b5c72
Pryesh Gohil <pryeshgohil@hotmail.com>
2022-11-28 22:54:57 +0000

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Pryesh Gohil on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
